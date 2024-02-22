63770341b883b23f5280caea3961fe21f40dd833
Ashkan Nori <159775597+ashkannori@users.noreply.github.com>
2024-02-23 01:20:51 +0330

    Update MyFiles.kt

--- src/main/kotlin/MyFiles.kt
+++ src/main/kotlin/MyFiles.kt
@@ -8,28 +8,20 @@ import kotlin.collections.ArrayList
 class MyFiles {
 
     fun compressFiles(paths: ArrayList<String>) {
-        var text: String = ""
-
         try {
-            for (item in paths) {
-                val fr = FileReader(item)
-                val sc = Scanner(fr)
-                while (sc.hasNext()) {
-                    text += sc.nextLine()
+            val text = paths.joinToString("\n-----\n") { path ->
+                FileReader(path).use { fr ->
+                    Scanner(fr).use { sc ->
+                        generateSequence { if (sc.hasNextLine()) sc.nextLine() else null }.joinToString("\n")
+                    }
                 }
-
-                fr.close()
-
-                text += "\n-----$item-----\n"  // at the end of the string, when the new file starts, use this text to separate the files
             }
 
-            val myFile = File("myFile.Ashkan")
-            val wr = FileWriter(myFile)
-            wr.write(text)
+            FileWriter(File("myFile.Ashkan")).use { wr ->
+                wr.write(text)
+            }
 
-            wr.close()
-
-            println("compressing files finished ...")        // This is a text to announce the end of the process
+            println("compressing files finished ...")
         } catch (e: Exception) {
             println("Error in compressFiles function in class MyFiles !!")
             println("your file have an Error !...")
@@ -39,25 +31,21 @@ class MyFiles {
 
     fun extractFiles(path: String, listOfFiles: ArrayList<String>) {
         try {
-            val fr = FileReader(path)
-            val sc = Scanner(fr)
-            for (item in listOfFiles) {
-                var saveInfo: String = ""
-                while (sc.hasNext()) {
-                    val temp = sc.nextLine()
-                    if (temp.equals("-----$item-----")) {
-                        break
-                    }
-                    saveInfo += temp
-                }
-                val myFile = File("extractFiles//$item")        // the path of extract files
-                val wr = FileWriter(myFile)
-                wr.write(saveInfo)
-                wr.close()
-            }
-            fr.close()
+            FileReader(path).use { fr ->
+                Scanner(fr).use { sc ->
+                    for (item in listOfFiles) {
+                        val saveInfo = generateSequence {
+                            if (sc.hasNextLine()) sc.nextLine() else null
+                        }.takeWhile { it != "-----$item-----" }.joinToString("\n")
 
-            println("extracting files finished ...")        // This is a text to announce the end of the process
+                        FileWriter(File("extractFiles//$item")).use { wr ->
+                            wr.write(saveInfo)
+                        }
+                    }
+                }
+            }
+
+            println("extracting files finished ...")
         } catch (e: Exception) {
             println("Error in extractFiles function in class MyFiles !!")
             println("your file have an Error !...")
